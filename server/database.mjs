40443bdf2f7d13ebc6a8430d1f04a1790aa41354
DavidoOwO <davidepinzan@libero.it>
2025-05-06 14:29:21 +0200

    Added script to populate database (collection: vocabulary)

--- server/database.mjs
+++ server/database.mjs
@@ -1,22 +1,50 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
-import { createRequire } from "module"
+import { createRequire } from "module";
 
-const require = createRequire(import.meta.url)
-const firebaseConfing = require("./config.json")
-const app = initializeApp(firebaseConfing)
-const firestore = getFirestore(app)
-const dictionary = require("./jmdictcommon.json")
+const require = createRequire(import.meta.url);
+const firebaseConfig = require("./config.json");
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const dictionary = require("./jmdictcommon.json");
 
 async function addTags() {
    for (let key in dictionary.tags) {
       try {
-         await setDoc(doc(firestore, "tags", key), {explanation: dictionary.tags[key]});
+         await setDoc(doc(firestore, "tags", key), {
+            explanation: dictionary.tags[key],
+         });
       } catch (e) {
          console.error("Error: ", e);
       }
-     console.log(dictionary.tags[key])
+      console.log(dictionary.tags[key]);
    }
 }
 
-addTags()
+async function addVocabulary(start, end) {
+   for (let i = start; i <= end; i++) {
+      var vocab = dictionary.words[i]
+      /*Nested arrays are not supported in Firebase. 
+      This gets rid of the only possible instances of such a thing by removing antonyms and related words, might find a way to include them in the future.*/
+      for (let j = 0; j < vocab.sense.length; j++) {
+         vocab.sense[j].antonym = []
+         vocab.sense[j].related = []
+      }
+      try {
+         await setDoc(doc(firestore, "vocabulary", vocab.id), vocab,  {merge: true});
+         console.log(vocab.kanji[0].text)
+      } catch (e) {
+         console.error("Error: ", e);
+      }
+   }
+}
+
+
+
+
+
+
+
+//addTags()
+
+//addVocabulary(process.argv[2], process.argv[3]);
