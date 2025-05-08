8ccc0900f7e0f2531be297e228ccd7a9be57e630
DavidoOwO <davidepinzan@libero.it>
2025-05-08 22:29:18 +0200

    Experiments with UI

--- client/lib/wordpage.dart
+++ client/lib/wordpage.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WordPage extends StatelessWidget {
-
   const WordPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("探査", style: TextStyle(fontFamily: "serif", fontWeight: FontWeight.w700, fontSize: 65.0))
+      body: Row(
+        spacing: 10.0,
+        children: [
+          IconButton(
+            icon: Icon(Icons.bookmark),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Added to collection.")),
+              );
+            },
+          ),
+          Text("直す", style: GoogleFonts.shipporiMincho(fontSize: 65.0)),
+          Text(
+            "(なおす)",
+            style: GoogleFonts.shipporiMincho(
+              fontSize: 35.0,
+              color: Color.fromARGB(201, 0, 0, 0),
+            ),
+          ),
+        ],
+      ),
     );
-  } 
-
+  }
 }
