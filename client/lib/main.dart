3f4c0877df67947533e7036d2284bd2a3f9e53a0
Zarkiom <zarkiom@gmail.com>
2025-05-09 15:45:23 +0200

    Added ThemeData

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -9,11 +9,57 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(        
+        colorScheme: ColorScheme(
+          brightness: Brightness.dark,
+          primary: const Color.fromARGB(255,200,233,233),
+          onPrimary: Colors.black,
+          secondary: Colors.cyan.shade300,
+          onSecondary: Colors.grey,
+          error: Colors.yellow,
+          onError: Colors.red,
+          surface: Colors.white,
+          onSurface: Colors.black,
         ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Main Page')),
+      body: Center(child: Text('Imparare a leggere in Giapponese, weeb')),
+      bottomNavigationBar: BottomNavigationBar(   
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Theme.of(context).colorScheme.inversePrimary,
+        selectedItemColor: Theme.of(context).colorScheme.inversePrimary,
+        items: [          
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Dizionario'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Parole Memorizzate',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
