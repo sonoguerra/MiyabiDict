c4d308da886f09b4a1cc9ae1f01477db818717e2
sonoguerra <151522030+sonoguerra@users.noreply.github.com>
2025-05-11 11:07:22 +0200

    searchbar now shows prev results

--- client/lib/dictionary.dart
+++ client/lib/dictionary.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:pwa_dict/dict_parser.dart';
-
-final dict = DictParser();
+import 'package:pwa_dict/dict_util.dart';
 
 class DictionaryList extends StatelessWidget{
   const DictionaryList({super.key});
   
   @override
   Widget build(BuildContext context) => _AsyncSearch();
-  
-  
 }
 
 class _AsyncSearch extends StatefulWidget {
@@ -18,33 +14,92 @@ class _AsyncSearch extends StatefulWidget {
 }
 
 class _AsyncSearchState extends State<_AsyncSearch> {
-  String? query;
+  String? _query;
+  List<String> _results = []; 
+  final List<String> _prevQueries = [];
+  late final SearchController _sc;
+
+  @override
+  void initState() {
+    super.initState();
+    _sc = SearchController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _sc.dispose();
+  }
+
+  //TODO
+  Future<void> _performSearch(String q) async {
+    // final fetchedResults = await DictUtil.readJson("assets/jmdict-eng-common-3.6.1.json");
+    setState(() {
+      _query = q;
+      if (!_prevQueries.contains(q)) {
+        _prevQueries.add(q);
+      }
+      _results = ['Result for "$q"']; // Replace with actual results
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SearchAnchor(
-      builder: (context, controller) {
-        return SearchBar(
-          controller: controller,
-          onTap: () { },
-          trailing: <Widget>[
-            Icon(Icons.search)
-          ],
-        );
-      },
-      suggestionsBuilder: (context, controller) {
-        return List<ListTile>.generate(5, (int index) {
-          final String item = 'item $index';
-          return ListTile(
-            title: Text(item),
+    return Column(
+      children: [
+      SearchAnchor(
+        builder: (context, _) {
+          return SearchBar(
+            leading: IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                final currentQuery = _sc.text;
+                if (currentQuery.isNotEmpty) {
+                  _performSearch(currentQuery);
+                }
+              },
+            ),
+            controller: _sc,
+            onSubmitted: (value) {
+              if (value.isNotEmpty) {
+                _performSearch(value);
+                _sc.text = value;
+              }
+            },
             onTap: () {
-              setState(() {
-                controller.closeView(item);
-              });
-            }
+              if (_prevQueries.isNotEmpty) {
+                _sc.openView();
+              }
+            },
           );
-        });
-      }
+        },
+        searchController: _sc,
+        suggestionsBuilder: (context, _) {
+          if (_prevQueries.isEmpty) {
+            return const <Widget>[];
+          }
+          return _prevQueries.map((res) {
+            return ListTile(
+              title: Text(res),
+              onTap: () {
+                _sc.closeView(res);
+                _performSearch(res);
+              },
+            );
+          }).toList();
+        },
+      ),
+      Expanded(
+        child: ListView.builder(
+          itemCount: _results.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(_results[index]),
+            );
+          },
+        ),
+      ),
+      ],
     );
   }
 }
