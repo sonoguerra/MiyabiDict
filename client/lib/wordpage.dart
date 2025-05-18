b500534871254c7036ad4fa6f348df720ff46fc5
DavidoOwO <davidepinzan@libero.it>
2025-05-18 21:27:20 +0200

    Managing UI changes based on viewport

--- client/lib/wordpage.dart
+++ client/lib/wordpage.dart
@@ -13,56 +13,55 @@ class WordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-              spacing: 10.0,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.arrow_back_rounded),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-                IconButton(
-                  icon: Icon(Icons.bookmark),
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Added to collection.")),
-                    );
-                  },
-                ),
-                Text(
-                  displayed.word,
-                  style: GoogleFonts.shipporiMincho(fontSize: 65.0),
-                ),
-                Tooltip(
-                  message: _kanaKit.toRomaji(displayed.kana[0].text),
-                  child: Text(
-                    displayed.word == displayed.kana[0].text
-
-
-                    
-                        ? ""
-                        : "【${displayed.kana[0].text}】",
-                    style: GoogleFonts.shipporiMincho(
-                      fontSize: 35.0,
-                      color: Color.fromARGB(201, 0, 0, 0),
-                    ),
+      body: Column(
+        children: [
+          Row(
+            spacing: 10.0,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back_rounded),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.bookmark),
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text("Added to collection.")),
+                  );
+                },
+              ),
+              Text(
+                displayed.word,
+                style: GoogleFonts.shipporiMincho(fontSize: 65.0),
+              ),
+              Tooltip(
+                message: _kanaKit.toRomaji(displayed.kana[0].text),
+                child: Text(
+                  displayed.word == displayed.kana[0].text
+      
+      
+                  
+                      
+                      ? ""
+                      : "【${displayed.kana[0].text}】",
+                  style: GoogleFonts.shipporiMincho(
+                    fontSize: 35.0,
+                    color: Color.fromARGB(201, 0, 0, 0),
                   ),
                 ),
-              ],
-            ),
-            ListView.separated(
-              itemCount: displayed.senses.length,
-              itemBuilder:
-                  (context, index) => Text(displayed.senses[index].glosses),
-              separatorBuilder: (context, index) => Divider(),
-              shrinkWrap: true,
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+          ListView.separated(
+            itemCount: displayed.senses.length,
+            itemBuilder:
+                (context, index) => Text(displayed.senses[index].glosses),
+            separatorBuilder: (context, index) => Divider(),
+            shrinkWrap: true,
+          ),
+        ],
       ),
     );
   }
