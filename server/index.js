64ddb1c7d7d5d98372cab4a246df197a6e85b8cd
DavidoOwO <davidepinzan@libero.it>
2025-06-01 17:21:57 +0200

    Fixed english search in backend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {kanjiAnywhere, readingBeginning, setup as setupJmdict} from 'jmdict-simplified-node'
+import {kanjiBeginning, readingBeginning, setup as setupJmdict} from 'jmdict-simplified-node'
 const app = express()
 
 const license = "These results are provided using JMDICT dictionary files (in particular the json simplified version at https://github.com/scriptin/jmdict-simplified). Both the original and simplified JMDICT files are made available under a CC-BY-SA 4.0 license. Refer to https://www.edrdg.org/wiki/index.php/JMdict-EDICT_Dictionary_Project for the sources and to https://creativecommons.org/licenses/by-sa/4.0/legalcode.en for the legal code of the license."
@@ -19,7 +19,7 @@ app.get('/search/reading/:reading', async (req, res) => {
 
 app.get('/search/kanji/:kanji', async (req, res) => {
    var w = req.params['kanji']
-   var results = await kanjiAnywhere(db, w)
+   var results = await kanjiBeginning(db, w)
    res.json({words: results, license: license})
 })
 
@@ -28,11 +28,13 @@ app.get('/search/english/:english', async (req, res) => {
    for (let i = 0x3040; i < 0x30A0; i++) {
       let results = await readingBeginning(db, String.fromCharCode(i))
       for (let j = 0; j < results.length; j++) {
-         for (let k = 0; k < results[j].sense.length; k++) {
-            for (let l = 0; l < results[j].sense[k].gloss.length; l++) {
+         let found = false
+         for (let k = 0; k < results[j].sense.length && !found; k++) {
+            for (let l = 0; l < results[j].sense[k].gloss.length && !found; l++) {
                let w = results[j].sense[k].gloss[l].text.toLowerCase()
                if (w.split(" ").includes(req.params['english'])) {
-                  query.push(results[j])
+                  found = true
+                  query.push((results[j]))
                }
             }
          }
@@ -45,4 +47,6 @@ app.get('/dictionary/common', (req, res) => res.sendFile("jmdictcommon.json", {r
 
 app.get('/dictionary', (req, res) => res.end(license))
 
-app.listen(port, () => console.log("Server running."))
+app.listen(port, () => {
+   console.log("Server running.")
+})
